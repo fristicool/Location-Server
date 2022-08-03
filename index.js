1e1f9d00c74c4cfbc63dd051951aa90a53feb8d7
fristicool <74139390+fristicool@users.noreply.github.com>
2022-08-03 20:44:38 +0200

    posrt vs 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(cors())
